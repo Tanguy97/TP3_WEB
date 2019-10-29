e7b623a2fe945264caf5327ecde5effafb30d907
YuYu <jinyutung@gmail.com>
2019-10-29 18:26:52 -0400

    first question

--- routes/rest/members.js
+++ routes/rest/members.js
@@ -5,10 +5,16 @@ module.exports = serviceTeam => {
 
   // À COMPLÉTER
   router.get('/',(req,res,next)=>{
-    serviceTeam.getTeamMembers({})((err,members)=>{
+    serviceTeam.getTeamMembers((err,members)=>{
+      req.app.locals.t
+      if(err){
+        if(t['ERRORS']['MEMBERS_ERROR']!= undefined)
+          res.json({errors: [t['ERRORS']['MEMBERS_ERROR']]})
+        else 
+          err.message
+      }
       res.json(members)
     })
   })
-
   return router
 }
