0bbc5a52a74285da284942374413c348e06f2466
YuYu <jinyutung@gmail.com>
2019-10-29 18:04:41 -0400

    first commit

--- routes/rest/members.js
+++ routes/rest/members.js
@@ -4,6 +4,11 @@ module.exports = serviceTeam => {
   const router = express.Router()
 
   // À COMPLÉTER
+  router.get('/',(req,res,next)=>{
+    serviceTeam.getTeamMembers({})((err,members)=>{
+      res.json(members)
+    })
+  })
 
   return router
 }
