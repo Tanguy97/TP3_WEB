b68e6cf3a153e5534db8a392504293d8503210fa
Tanguy Gloaguen <tanguy.gloaguen@grenoble-inp.org>
2019-10-31 15:08:59 -0400

    members

--- routes/rest/members.js
+++ routes/rest/members.js
@@ -6,12 +6,13 @@ module.exports = serviceTeam => {
   // À COMPLÉTER
   router.get('/',(req,res,next)=>{
     serviceTeam.getTeamMembers((err,members)=>{
-      req.app.locals.t
       if(err){
-        if(t['ERRORS']['MEMBERS_ERROR']!= undefined)
-          res.json({errors: [t['ERRORS']['MEMBERS_ERROR']]})
-        else 
-          err.message
+        if(req.app.locals.t['ERRORS']['MEMBERS_ERROR']!= undefined){
+          res.status(500).json({errors: [req.app.locals.t['ERRORS']['MEMBERS_ERROR']]})
+        }
+        else{
+          res.status(500).json(err.message)
+        }
       }
       res.json(members)
     })
