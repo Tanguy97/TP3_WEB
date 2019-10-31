8a3e672ed7233e94262e181191fb4acbc109bb75
YuYu <jinyutung@gmail.com>
2019-10-31 17:14:44 -0400

    1

--- routes/rest/members.js
+++ routes/rest/members.js
@@ -7,19 +7,12 @@ module.exports = serviceTeam => {
   router.get('/',(req,res,next)=>{
     serviceTeam.getTeamMembers((err,members)=>{
       if(err){
-<<<<<<< HEAD
-        if(t!= undefined)
-          res.json({errors: [t['ERRORS']['MEMBERS_ERROR']]})
-        else 
-          err.message
-=======
         if(req.app.locals.t['ERRORS']['MEMBERS_ERROR']!= undefined){
           res.status(500).json({errors: [req.app.locals.t['ERRORS']['MEMBERS_ERROR']]})
         }
         else{
           res.status(500).json({errors: [err.message]})
         }
->>>>>>> b68e6cf3a153e5534db8a392504293d8503210fa
       }
       res.json(members)
     })
