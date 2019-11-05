1d890fe3eacf9be14a66d5b9c5656df97f0a78e3
Tanguy Gloaguen <tanguy.gloaguen@grenoble-inp.org>
2019-11-05 14:34:50 -0500

    ajout team.js

--- routes/team.js
+++ routes/team.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
 
 // À COMPLÉTER
-
+router.get('/',(req,res,next)=>{
+  const url = 'http://localhost:3000/api/members'
+  request.get(url,(err,resultat,body)=>{
+    if(err){
+      next(err)
+    }
+    const team = JSON.parse(body)
+    res.render('team.pug',{"members": team})
+  })
+})
 module.exports = router
