12476aa17dfc97a2b79418607774bf3787831c7f
Tanguy Gloaguen <tanguy.gloaguen@grenoble-inp.org>
2019-11-05 14:50:34 -0500

    ajout projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
 
 // À COMPLÉTER
-
+router.get('/',(req,res,next)=>{
+  const url = 'http://localhost:3000/api/projects'
+  request.get(url,(err,resultat,body)=>{
+    if(err){
+      next(err)
+    }
+    const projects = JSON.parse(body).map(val=>val["project"])
+    res.render('projects.pug',{"projects": projects})
+  })
+})
 module.exports = router
