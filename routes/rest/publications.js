9b8802f8b447a6ced4e893bc243c98885a75acda
YuYu <jinyutung@gmail.com>
2019-11-07 21:46:37 -0500

    fix

--- routes/rest/publications.js
+++ routes/rest/publications.js
@@ -55,35 +55,70 @@ module.exports = servicePublication => {
     let venue = req.body.venue
 
     if (title == undefined || month === undefined || year == undefined || authors == undefined || venue == undefined ){
-      res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['PUB_CREATE_ERROR']] })
       console.log(title)
+      if(req.app.locals.t===undefined){
+        if( req.app.locals.t['ERRORS']['PUB_CREATE_ERROR'] ===undefined) res.status(400).json({ errors: [err.message] })
+        else res.status(400).json({ errors: [err.message] })
+      }
+      else res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['PUB_CREATE_ERROR']] })
     }
     //error code 400 bad request
     
     else if (title.length < 5){
-      res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM'] ]})
       console.log(title)
+      if(req.app.locals.t===undefined){
+        if( req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM'] ===undefined) res.status(400).json({ errors: [err.message] })
+        else res.status(400).json({ errors: [err.message] })
+      }
+      else res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM'] ]})
     }      
-    else if (month > 0 && month > 11)
-      res.status(400).json({ errors: [req.app.locals.t['ERRORS']['MONTH_ERROR_FORM']] })
-    else if (year.match(/[0-9]+/g))
-      res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM']] })
-    else if (venue.length < 5)
-      res.status(400).json({ errors: [req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM']] })
-    else if (est_vide(authors))
-      res.status(400).json({ errors: [req.app.locals.t['ERRORS']['AUTHOR_EMPTY_ERROR']] })
+    else if (month > 0 && month > 11){
+      console.log(month)
+      if(req.app.locals.t===undefined){
+        if( req.app.locals.t['ERRORS']['MONTH_ERROR_FORM'] ===undefined) res.status(400).json({ errors: [err.message] })
+        else res.status(400).json({ errors: [err.message] })
+      }
+      else res.status(400).json({ errors: [req.app.locals.t['ERRORS']['MONTH_ERROR_FORM']] })
+    }
+      
+    else if( year.toString().match(/[0-9]+/g)){
+      console.log(year)
+      if(req.app.locals.t===undefined){
+        if( req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM'] ===undefined) res.status(400).json({ errors: [err.message] })
+        else res.status(400).json({ errors: [err.message] })
+      }
+      else res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM']] })
+    }
+      
+    else if (venue.length < 5){
+      console.log(venue)
+      if(req.app.locals.t===undefined){
+        if( req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM'] ===undefined) res.status(400).json({ errors: [err.message] })
+        else res.status(400).json({ errors: [err.message] })
+      }
+      else res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM']] })
+    }
+
+    else if (est_vide(authors)){
+      console.log(authors)
+      if(req.app.locals.t===undefined){
+        if( req.app.locals.t['ERRORS']['AUTHOR_EMPTY_ERROR'] ===undefined) res.status(400).json({ errors: [err.message] })
+        else res.status(400).json({ errors: [err.message] })
+      }
+      else res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['AUTHOR_EMPTY_ERROR']] })
+    }
+
     else {
       const publication = { title: title, month: month, year: year, authors: authors, venue: venue }
 
       servicePublication.createPublication(publication)((err, publications) => {
 
-      if (err) {
-        if (req.app.locals.t['ERRORS']['PUB_CREATE_ERROR'] != undefined) {
-          res.status(500).json({ errors: [req.app.locals.t['ERRORS']['PUBS_ERROR']] })
+      if (err){
+        if(req.app.locals.t===undefined){
+          if( req.app.locals.t['ERRORS']['PUB_CREATE_ERROR'] ===undefined) res.status(400).json({ errors: [err.message] })
+          else res.status(400).json({ errors: [err.message] })
         }
-        else {
-          res.status(500).json({ errors: [err.message] })
-        }
+        else res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['PUB_CREATE_ERROR']] })
       }
       else res.status(201).send()
       })
