a01cafd0d2103609d88e8df8bbb281fa447ff582
Tanguy Gloaguen <tanguy.gloaguen@grenoble-inp.org>
2019-11-09 19:20:31 -0500

    création bd mongodb terminé

--- setup.js
+++ setup.js
@@ -23,21 +23,53 @@ async function main(){
 
     //Création de la collection news
     await db.createCollection('news')
-    const newsYaml = yaml.safeLoad(fs.readFileSync('./data/news.yml', 'utf8'))
-    const news = newsYaml.map(s => {
-      const newCreatedAtDate = moment(s.createdAt, 'YYYY-MM-DD HH:mm:ss').toDate()
-      return {
-        ...s,
-        text: {fr: s.text.fr, en: s.text.en},
-        type: 'news',
-        createdAt: newCreatedAtDate
-      }
-    })
+    const news = yaml.safeLoad(fs.readFileSync('./data/news.yml', 'utf8'))
     await db.collection('news').insertMany(news)
     console.log('Collection news créée')
 
     //Création de la collection seminars
+    await db.createCollection('seminars')
+    const seminars = yaml.safeLoad(fs.readFileSync('./data/seminars.yml', 'utf8'))
+    await db.collection('seminars').insertMany(seminars)
+    console.log('Collection seminars créée')
 
+    //Création de la collection members
+    await db.createCollection('members')
+    const members = yaml.safeLoad(fs.readFileSync('./data/team.yml', 'utf8'))
+    await db.collection('members').insertMany(members)
+    console.log('Collection members créée')
+
+    //Création de la collection publication
+    await db.createCollection('publications')
+    const publications = yaml.safeLoad(fs.readFileSync('./data/publications.yml', 'utf8'))
+    await db.collection('publications').insertMany(publications)
+    console.log('Collection publications créée')
+
+    //Création de la collection projects
+    await db.createCollection('projects')
+    const projectsBadId = yaml.safeLoad(fs.readFileSync('./data/projects.yml', 'utf8'))
+    const projects = await Promise.all(projectsBadId.map(async project=>{
+      if(project['publications']!=undefined){
+        try{
+          project['publications'] = await Promise.all(project['publications'].map(async key =>{
+            try{
+              const id = await db.collection('publications').findOne({key: key},{projection: {_id: 1}})
+              return id['_id']
+            }catch(err) {
+              console.log(err.stack)
+            }
+          }))
+        }catch(err) {
+          console.log(err.stack)
+        }
+      }
+      return project
+    }))
+    await db.collection('projects').insertMany(projects)
+    console.log('Collection projects créée')
+
+    //Suppression des key de publication
+    await db.collection('publications').updateMany({},{$unset:{key:""}})
 
   } catch (err) {
     console.log(err.stack)
