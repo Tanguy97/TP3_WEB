439acf30d5f33b85b3ee8f29cc042529ccd35b3b
jytung <jinyutung@gmail.com>
2019-11-11 10:48:58 -0500

    tout les tests passentgit add routes/rest/publications.js !

--- routes/rest/publications.js
+++ routes/rest/publications.js
@@ -52,6 +52,7 @@ module.exports = servicePublication => {
     let year
     let authors
     let venue
+    let error=[]
     if(req.body!=undefined){
       title = req.body.title
       month = req.body.month
@@ -60,48 +61,51 @@ module.exports = servicePublication => {
       venue = req.body.venue
       if (title == undefined && month === undefined && year == undefined && authors == undefined && venue == undefined ){
         if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['PUB_CREATE_ERROR'] !==undefined){
-          res.status(500).json({ errors:[ req.app.locals.t['ERRORS']['PUB_CREATE_ERROR']] })
+          error.push(req.app.locals.t['ERRORS']['PUB_CREATE_ERROR'])
         }
-        else res.status(500).json({ errors: ['PUB_CREATE_ERROR'] })
+        else error.push('PUB_CREATE_ERROR')
       }
 
-      else if (title===undefined||title.length < 5 ){
+      if (title===undefined||title.length < 5 ){
         if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM'] !==undefined){
-          res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM']] })
+          error.push(req.app.locals.t['ERRORS']['PUB_AT_LEAST_5_CHAR_FORM'])
         }
-        else res.status(400).json({ errors: ['PUB_AT_LEAST_5_CHAR_FORM'] })
+        else error.push('PUB_AT_LEAST_5_CHAR_FORM')
       }
 
-      else if (month===undefined||(month < 0 || month > 11)||month.toString().match(/[0-9]+/g)===null  ){
+      if (month===undefined||(month < 0 || month > 11)||month.toString().match(/[0-9]+/g)===null  ){
 
         if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['MONTH_ERROR_FORM'] !==undefined){
-          res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['MONTH_ERROR_FORM']] })
+          error.push(req.app.locals.t['ERRORS']['MONTH_ERROR_FORM'])
         }
-        else res.status(400).json({ errors: ['MONTH_ERROR_FORM'] })
+        else error.push('MONTH_ERROR_FORM')
       }
 
-      else if( year===undefined ||year.toString().match(/[0-9]+/g)===null ){
+      if( year===undefined ||year.toString().match(/[0-9]+/g)===null ){
         if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM'] !==undefined){
-          res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM']] })
+          error.push(req.app.locals.t['ERRORS']['YEAR_NOT_INT_FORM'])
         }
-        else res.status(400).json({ errors: ['YEAR_NOT_INT_FORM'] })
+        else error.push('YEAR_NOT_INT_FORM')
       }
 
-      else if (venue===undefined  || venue.length < 5){
+      if (venue===undefined  || venue.length < 5){
         if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM'] !==undefined){
-          res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM']] })
+          error.push(req.app.locals.t['ERRORS']['VENUE_AT_LEAST_5_CHAR_FORM'])
         }
-        else res.status(400).json({ errors: ['VENUE_AT_LEAST_5_CHAR_FORM'] })
+        else error.push('VENUE_AT_LEAST_5_CHAR_FORM')
       }
 
-      else if (authors==undefined ||est_vide(authors)  ||  authors.length==0){
+      if (authors==undefined ||est_vide(authors)  ||  authors.length==0){
         if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['AUTHOR_EMPTY_FORM'] !==undefined){
-          res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['AUTHOR_EMPTY_FORM']] })
+          error.push(req.app.locals.t['ERRORS']['AUTHOR_EMPTY_FORM'])
         }
-        else res.status(400).json({ errors: ['AUTHOR_EMPTY_FORM'] })
+        else error.push('AUTHOR_EMPTY_FORM')
       }
-
-      else {
+      
+      if (error.length!=0){
+        res.status(400).json({ errors:error })
+      }
+      else{
         const publication = { title: title, month: month, year: year, authors: authors, venue: venue }
         servicePublication.createPublication(publication)((err, publications) => {
         if (err){
@@ -113,8 +117,9 @@ module.exports = servicePublication => {
         else res.status(201).json(publications)
         })
       }
+      
     }
-    else {
+    else { 
       if(req.app.locals.t!==undefined && req.app.locals.t['ERRORS'] !==undefined && req.app.locals.t['ERRORS']['EMPTY_PUBLICATION_FORM'] !==undefined){
         res.status(400).json({ errors:[ req.app.locals.t['ERRORS']['EMPTY_PUBLICATION_FORM']] })
       }
