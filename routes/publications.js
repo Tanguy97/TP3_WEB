ee3cd75ff8712da5c297e2d45dc5e50bf5783327
jytung <jinyutung@gmail.com>
2019-11-12 14:05:08 -0500

    pub route pbl...

--- routes/publications.js
+++ routes/publications.js
@@ -16,13 +16,14 @@ router.get('/', (req, res, next) => {
   if (limit === undefined) limit = 10
   if (order_by === undefined) order_by = "desc"
   if (sort_by === undefined) sort_by = "date"
-  const url = 'http://localhost:3000/api/publications'
+  const url = 'http://localhost:3000/api/publications'+'?limit='+limit+'&page='+page+'&sort_by='+sort_by+'&order_by='+order_by
   request.get(url, (err, resultat, body) => {
     if (err) {
       error = JSON.parse(body)
       next(err)
     }
     publication = JSON.parse(body)["publication"]
+    const count = JSON.parse(body)["count"]
     if (sort_by === undefined) sort_by = "date"
     else if (sort_by === "date") {
       var sorting_tab = [[publication.year, sort_by], [publication.month, sort_by]]
@@ -31,12 +32,7 @@ router.get('/', (req, res, next) => {
       var sorting_tab = [[publication.title, sort_by]]
     }
     const pageOpt = { "pageNumber": page, "limit": limit, "sorting": sorting_tab }
-    const numPage = Math.ceil(publication.length / limit)
-    let langue = req.query.clang
-    if (langue === undefined) {
-      langue = 'fr'
-    }
-    moment.locale(langue)
+    const numPage = Math.ceil(count/ limit)
     const month = moment.months()
     res.render('publication.pug', { "publications": publication, "pubFormErrors": error, "pagingOptions": pageOpt, "numberOfPages": numPage, "monthNames": month })
   })
@@ -49,39 +45,6 @@ router.post('/', (req, res, next) => {
       error = JSON.parse(body)
       next(err)
     }
-  })
-  let error = []
-  let publication
-  let page = req.query.page
-  let limit = req.query.limit
-  let order_by = req.query.order_by
-  let sort_by = req.query.sort_by
-  if (page === undefined) page = 1
-  if (limit === undefined) limit = 10
-  if (order_by === undefined) order_by = "desc"
-  if (sort_by === undefined) sort_by = "date"
-  request.get(url, (err, resultat, body) => {
-    if (err) {
-      error = JSON.parse(body)
-      next(err)
-    }
-    publication = JSON.parse(body)["publication"]
-    if (sort_by === undefined) sort_by = "date"
-    else if (sort_by === "date") {
-      var sorting_tab = [[publication.year, sort_by], [publication.month, sort_by]]
-    }
-    else if (sort_by === "title") {
-      var sorting_tab = [[publication.title, sort_by]]
-    }
-    const pageOpt = { "pageNumber": page, "limit": limit, "sorting": sorting_tab }
-    const numPage = Math.ceil(publication.length / limit)
-    let langue = req.query.clang
-    if (langue === undefined) {
-      langue = 'fr'
-    }
-    moment.locale(langue)
-    const month = moment.months()
-    res.render('publication.pug', { "publications": publication, "pubFormErrors": error, "pagingOptions": pageOpt, "numberOfPages": numPage, "monthNames": month })
   })
 })
 
